eba2f49cacd0456ae655accab59ea23297a2dd70
Neelu Chandra <86164992+neelu43@users.noreply.github.com>
2022-11-02 17:38:11 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ import './App.css'
 export default function App() {
   return (
     <main>
-      React ⚛️ + Vite ⚡ + Replit 🌀
+     <div>
+     <ul>
+     <li>Home </li>
+     <li>About</li>
+     <li>Contact</li>
+     <li>Testimonials</li>
+     <li>Footer</li></ul>
+      </div>
     </main>
   )
 }
